ca661841d1ccae644682bef1917f1909c894d930
Ana Silva <ananunesdasilva@gmail.com>
2019-03-17 18:41:10 +0000

    Refactor Expense model test

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -1,7 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Expense, type: :model do
-  subject { described_class.new(amount: 1) }
+  subject do
+    described_class.new(
+      amount: 1,
+      expense_date: Date.today,
+      description: 'dummy',
+      category: 1
+    )
+  end
 
   it 'is valid with valid attributes' do
     expect(subject).to be_valid
